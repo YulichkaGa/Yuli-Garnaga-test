a36e21b69fe228026cb42e15896aeeaa67146505
Yuli <yulikga@gmail.com>
2024-01-21 22:19:41 +0200

    add all

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer.Server;
+using System.Collections.Generic;
 using Yuli_Garnaga_test.Data;
+using Yuli_Garnaga_test.Models;
 
 namespace Yuli_Garnaga_test.Controllers
 {
@@ -21,7 +24,7 @@ namespace Yuli_Garnaga_test.Controllers
             return Ok(customers);
         }
         [HttpGet("{title}")]
-        public async Task<IActionResult> GetQuestions([FromRoute] string title)
+        public async Task<IActionResult> GetAllQuestionsByTitle([FromRoute] string title)
         {
             var question = await _contextDb.Questions.Where(x => x.Title == title).ToListAsync();
             if (question == null)
@@ -31,9 +34,9 @@ namespace Yuli_Garnaga_test.Controllers
             return Ok(question);
         }
         [HttpGet]
-        public async Task<IActionResult> GetAllQAndA()
+        public async Task<IActionResult> GetAllQuestionsAnswers()
         {
-            var query =  (from q in _contextDb.Questions
+            var query = (from q in _contextDb.Questions
                          join a in _contextDb.Answers on q.Id equals a.QId
 
                          select new
@@ -49,6 +52,42 @@ namespace Yuli_Garnaga_test.Controllers
 
             return Ok(all);
         }
+        [HttpGet]
+        public async Task<IActionResult> GetAnswerToTheQuestion()
+        {
+            var query = from question in _contextDb.Questions
+                        join answer in _contextDb.Answers on question.Id equals answer.QId
+                        join type in _contextDb.Types on question.TypeId equals type.Id
+                        select new
+                        {
+                            QuestionId = question.Id,
+                            QuestionDescription = question.Description_Q,
+                            AnswerDescription = answer.Description
+                        };
+            var result = await query.ToListAsync();
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCustomer([FromBody] Questions questionRequest)
+        {
+
+            await _contextDb.Questions.AddAsync(questionRequest);
+            await _contextDb.SaveChangesAsync();
+            return CreatedAtRoute( new { id = questionRequest.Id }, questionRequest);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetIfAnswerIsCorrect(string answerName)
+        {
+            var query = from answer in _contextDb.Answers
+                        where answer.Description.Contains(answerName)
+                        select answer;
+            var result = await query.ToListAsync();
+            return Ok(result);
+
+        }
     }
 }
 
+
+
+            
